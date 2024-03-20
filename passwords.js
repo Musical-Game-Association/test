9c0fffe65e50b98cd6fbc200bf3d51fce70bb23e
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 20:47:14 +0800

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,14 +1,11 @@
-// verifyPassword.js
-
 function verifyPassword(password) {
     switch(password) {
-        case '0220':
-            window.location.href = 'https://score.mugassn.com';
+        case 'proxy':
+            window.location.href = 'https://proxy.techny.site/155d9be2-322c-4f71-8d84-4d655701ce40';
             break;
-        case '1234': // 假设另一个密码是 '1234'，并将其跳转到另一个网站
+        case '1234':
             window.location.href = 'https://example.com';
             break;
-        // 添加更多密码和相应的跳转网站
         default:
             alert('未查询到存在该验证码，请核实！');
     }
