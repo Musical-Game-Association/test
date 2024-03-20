96334d920817283006a46f5752825d88b8739c7f
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 15:15:03 +0800

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,12 +1,15 @@
+var passwordMap = {
+    'Jhaan': 'https://www.mugassn.com',
+    'Kevin': 'https://www.kevinwu.cc',
+    'Ben': 'https://timewaster43.github.io',
+    'NewPassword': 'https://www.newwebsite.com'
+};
+
+// 导出函数以验证密码并进行相应的跳转
 function verifyPassword(password) {
-    if (password === 'Jhaan') {
-        window.location.href = 'https://www.mugassn.com';
-    } else if (password === 'Kevin') {
-        window.location.href = 'https://www.kevinwu.cc';
-    } else if (password === 'Ben') {
-        window.location.href = 'https://timewaster43.github.io';
-    } else if (password === 'NewPassword') {
-        window.location.href = 'https://www.newwebsite.com';
+    var url = passwordMap[password];
+    if (url) {
+        window.location.href = url;
     } else {
         alert('未查询存在该识别码，请核实！');
     }
