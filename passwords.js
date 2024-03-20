153143155041ef88fa8454f3a7de9644e9da5a5c
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 15:09:42 +0800

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,6 +1,13 @@
-var passwords = {
-    "Jhaan": "https://www.mugassn.com",
-    "Kevin": "https://www.kevinwu.cc",
-    "Ben": "https://timewaster43.github.io",
-    "NewPassword": "https://www.example.com"
-};
+function verifyPassword(password) {
+    if (password === 'Jhaan') {
+        window.location.href = 'https://www.mugassn.com';
+    } else if (password === 'Kevin') {
+        window.location.href = 'https://www.kevinwu.cc';
+    } else if (password === 'Ben') {
+        window.location.href = 'https://timewaster43.github.io';
+    } else if (password === 'NewPassword') {
+        window.location.href = 'https://www.newwebsite.com';
+    } else {
+        alert('未查询存在该识别码，请核实！');
+    }
+}
