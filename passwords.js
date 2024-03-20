defeed57046efd37c6d109763fb1102c95b342b6
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 21:04:57 +0800

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,12 +1,17 @@
-function verifyPassword(password) {
-    switch(password) {
-        case 'proxy':
-            window.location.href = 'https://proxy.techny.site/155d9be2-322c-4f71-8d84-4d655701ce40';
-            break;
-        case '1234':
-            window.location.href = 'https://example.com';
-            break;
-        default:
-            alert('未查询到存在该验证码，请核实！');
+// 密码和对应的网址存储在一个对象中
+var passwordUrls = {
+    "0220": "https://score.mugassn.com",
+    "1234": "https://example.com"
+    // 可以继续添加更多的密码和对应的网址
+};
+
+// Function to verify the password and redirect to the corresponding URL
+function verifyPassword() {
+    var password = document.getElementById('password').value;
+    var url = passwordUrls[password];
+    if (url) {
+        window.location.href = url; // 如果密码存在于对象中，则跳转至对应网址
+    } else {
+        alert('密码错误！');
     }
 }
