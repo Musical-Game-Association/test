427912382d8a0831d6231c4f9599e4ade5978e7d
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 15:03:02 +0800

    Create passwords.js

---
+++ passwords.js
@@ -0,0 +1,6 @@
+var passwords = {
+    "Jhaan": "https://www.mugassn.com",
+    "Kevin": "https://www.kevinwu.cc",
+    "Ben": "https://timewaster43.github.io",
+    "NewPassword": "https://www.example.com"
+};
