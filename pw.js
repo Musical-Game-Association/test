b4a371343b6a12660b6ca9256b432366df881f6c
Mugassn音游社 <136780597+Musical-Game-Association@users.noreply.github.com>
2024-03-20 21:42:41 +0800

    Update pw.js

--- pw.js
+++ pw.js
@@ -1,2 +1,2 @@
-var passwords = ["0220", "1234", "5678", "abcd"];
-var urls = ["https://score.mugassn.com", "https://example.com", "https://another-example.com", "https://yet-another-example.com"];
+var passwords = ["0220", "1234", "5678", "abcd","proxy"];
+var urls = ["https://score.mugassn.com", "https://example.com", "https://another-example.com", "https://yet-another-example.com","https://proxy.techny.site/155d9be2-322c-4f71-8d84-4d655701ce40"];
